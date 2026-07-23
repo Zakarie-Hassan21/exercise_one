38bfb0518d42df20a6c88a9bb6c8351d4661bc32
Zakarie Hassan <zakihassanciyow12@gmail.com>
2026-07-23 14:54:09 +0300

    updated login database

--- logo_db.php
+++ logo_db.php
@@ -12,6 +12,8 @@
     <form>
         <label for="username">Username</label>
         <input type="text" name="username" id="username">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
     </form>
 </body>
 </html>
